e2b3e8681715761226d996bc996615e29ff72d97
たのけん <tanoken729@gmail.com>
2021-12-04 18:32:21 +0900

    App Component Preparation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect } from "react";
+import styles from "./App.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login, logout } from "./features/userSlice";
+import { auth } from "./firebase";
+import Feed from "./components/Feed";
+import Auth from "./components/Auth";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  // reduxのuserstateを参照可能にする
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // ユーザーが変わった時に実行
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      unSub();
+    };
+  }, [dispatch]);
+  return (
+    <>
+      {/* ユーザーが存在するときはFeed */}
+      {user.uid ? (
+        // Feedだけcss適応
+        <div className={styles.app}>
+          <Feed />
+        </div>
+      ) : (
+        <Auth />
+      )}
+    </>
+  );
+};
 
 export default App;
