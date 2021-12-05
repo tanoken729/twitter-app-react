0a4eae8d836326f61e54d6b9f1c465d96259c855
たのけん <tanoken729@gmail.com>
2021-12-05 18:50:29 +0900

    Tweet posting function

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -3,12 +3,71 @@ import styles from "./TweetInput.module.css";
 import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
 import firebase from "firebase/app";
-import { Avatar } from "@material-ui/core";
-import { auth } from "../firebase";
+import { Avatar, Button, IconButton } from "@material-ui/core";
+import { storage, db, auth } from "../firebase";
+import AddAPhotoIcon from "@material-ui/icons/AddAPhoto";
 
 const TweetInput = () => {
   // reduxのstateからuserstateを取得し、ローカルのuser変数に格納
   const user = useSelector(selectUser);
+  const [tweetImage, setTweetImage] = useState<File | null>(null);
+  const [tweetMsg, setTweetMsg] = useState("");
+  const onChangeImageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files![0]) {
+      setTweetImage(e.target.files![0]);
+      e.target.value = "";
+    }
+  };
+  const sendTweet = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (tweetImage) {
+      const S =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      const N = 16;
+      const randomChar = Array.from(crypto.getRandomValues(new Uint32Array(N)))
+        .map((n) => S[n % S.length])
+        .join("");
+      const fileName = randomChar + "_" + tweetImage.name;
+      // ツイート画像イメージ（返り値）格納
+      const uploadTweetImg = storage.ref(`images/${fileName}`).put(tweetImage);
+      // storageに変化があった場合の後処理
+      uploadTweetImg.on(
+        firebase.storage.TaskEvent.STATE_CHANGED,
+        // プログレス今回は空
+        () => {},
+        // エラーハンドリング
+        (err) => {
+          alert(err.message);
+        },
+        // 成功した場合
+        async () => {
+          await storage
+            .ref("images")
+            .child(fileName)
+            .getDownloadURL()
+            .then(async (url) => {
+              await db.collection("posts").add({
+                avatar: user.photoUrl,
+                image: url,
+                text: tweetMsg,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                username: user.displayName,
+              });
+            });
+        }
+      );
+    } else {
+      db.collection("posts").add({
+        avatar: user.photoUrl,
+        image: "",
+        text: tweetMsg,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        username: user.displayName,
+      });
+    }
+    setTweetImage(null);
+    setTweetMsg("");
+  };
   return (
     <div>
       <Avatar
